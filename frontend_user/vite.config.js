bb629dc0558d8f4e269a9491dde7b54598bfa8b8
1Light <redclover1.618@gmail.com>
2025-03-31 11:28:33 +0300

    create guest landing page

--- frontend_user/vite.config.js
+++ frontend_user/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",   // Allow external access
+    port: 8080,   // Set the development server port
+  },
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(), // Only enable in dev mode
+  ].filter(Boolean), // Remove falsy values
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Set alias for imports
+    },
+  },
+}));
