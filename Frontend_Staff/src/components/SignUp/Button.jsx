619155d7eb833e985f3bd221b54b81a44ef405cb
Mikias Berhanu <bmikias482@gmail.com>
2025-03-31 14:28:29 +0300

    signup page

--- Frontend_Staff/src/components/SignUp/Button.jsx
+++ Frontend_Staff/src/components/SignUp/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ children, type = "button", onClick }) => (
     <button
       type={type}
       onClick={onClick}
-      className="w-full bg-blue-600 text-white p-2 rounded-[3px] hover:bg-blue-700 transition my-4 cursor-pointer"
+      className="w-full bg-blue-600 text-white p-2 rounded-4xl hover:bg-blue-700 transition my-1  cursor-pointer"
     >
       {children}
     </button>
