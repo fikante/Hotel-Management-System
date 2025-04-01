fea5a6afb230ee499503f4f535ad33e29e018e33
Nebiyu46 <nebiyuent@gmail.com>
2025-04-01 03:15:56 +0300

    Auth implemnent

--- backend/src/v1/auth/dto/change-password.dto.ts
+++ backend/src/v1/auth/dto/change-password.dto.ts
@@ -4,7 +4,7 @@ export class ChangePasswordDto {
   @IsNotEmpty()
   oldPassword: string;
 
-  // makes sure that the new password is not empty and has a minimum length of 8 characters;
+  // makes sure that the new password is not empty and has a minimum length of 8 characters
   @IsNotEmpty()
   @MinLength(8)
   newPassword: string;
