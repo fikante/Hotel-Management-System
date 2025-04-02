9cde095e1821dcd99c3264713267379248a942fd
1Light <redclover1.618@gmail.com>
2025-04-02 08:02:39 +0300

    create billing page

--- frontend_user/vite.config.js
+++ frontend_user/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { componentTagger } from 'lovable-tagger';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,8 +10,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(), // Only enable in dev mode
-  ].filter(Boolean), // Remove falsy values
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Set alias for imports
