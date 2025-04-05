2dcbfa5c83cfd9281d20a01146cb087de6e03559
1Light <redclover1.618@gmail.com>
2025-04-05 10:48:48 +0300

    add payment modal

--- frontend_user/src/App.jsx
+++ frontend_user/src/App.jsx
@@ -18,7 +18,7 @@ import BrowseHotels from "./pages/BrowseHotels";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
-import Payment from "./components/Payment/PaymentModal";
+import TestPaymentModal from "./pages/TestPaymentModals";
 
 import Menu from './pages/restaurant/Menu';
 
@@ -45,7 +45,7 @@ const App = () => (
             <Route path="/signup" element={<SignUp />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/payment" element={<Payment />} />
+            <Route path="/payment_modal" element={<TestPaymentModal />} />
             {/* These routes would be implemented later as the application grows */}
             <Route path="/user_rooms" element={<NotFound />} />
             <Route path="/user_rooms/:id" element={<NotFound />} />
