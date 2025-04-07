57bd996bff7d977a08d74086076dcbbe933dc7f5
Ley-code <leykunbi12@gmail.com>
2025-04-07 17:45:38 +0300

    dashboard added

--- backend/src/v1/hms/dashboard/dashboard.module.ts
+++ backend/src/v1/hms/dashboard/dashboard.module.ts
@@ -4,6 +4,7 @@ import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 import { Booking } from '../../../common/entities/booking.entity';
 import { Transaction } from '../../../common/entities/transaction.entity';
+import { Room } from 'src/common/entities/room.entity';
 
 /**
  * Dashboard Module - Provides hotel analytics and reporting functionality
@@ -17,7 +18,7 @@ import { Transaction } from '../../../common/entities/transaction.entity';
  * Requires TypeORM entities: Booking and User
  */
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking, Transaction])],
+  imports: [TypeOrmModule.forFeature([Booking, Transaction, Room])],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
