eea0d259c4c09482b087bbe712c8c63155a55d2e
Ley-code <leykunbi12@gmail.com>
2025-04-07 18:05:57 +0300

    merge conflicts

--- Frontend_Staff/src/pages/Food/AddFood.jsx
+++ Frontend_Staff/src/pages/Food/AddFood.jsx
@@ -44,6 +44,7 @@ const AddFood = ({ onSuccess }) => {
       alert("Please upload an image before submitting.");
       return;
     }
+<<<<<<< HEAD
     const IngredientArray = foodItem.ingredients
       .split(",")
       .map((item) => item.trim());
@@ -52,6 +53,19 @@ const AddFood = ({ onSuccess }) => {
     formData.append("ingredients", JSON.stringify(IngredientArray));
     formData.append("name", foodItem.name);
     formData.append("category", foodItem.category);
+=======
+
+    const ingredientsArray = foodItem.ingredients
+      .split(",")
+      .map((item) => item.trim());
+    const ingredientsJson = ingredientsArray.map((ingredient) => {
+      return { name: ingredient };
+    });
+    const formData = new FormData();
+    formData.append("ingredients", JSON.stringify(ingredientsJson));
+    formData.append("name", foodItem.name);
+    formData.append("categories", foodItem.category);
+>>>>>>> fb05b12 (merge conflicts merged)
     formData.append("timeToMake", foodItem.preparationTime);
     formData.append("price", foodItem.price);
     formData.append("image", foodItem.image);
@@ -66,9 +80,12 @@ const AddFood = ({ onSuccess }) => {
       });
 
       console.log(response.data);
+<<<<<<< HEAD
       setError(null);
       onSuccess();
       alert("Food item added successfully!");
+=======
+>>>>>>> fb05b12 (merge conflicts merged)
     } catch (error) {
       console.error("Error adding food item:", error);
       setError("Failed to add food item. Please try again.");
