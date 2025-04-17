2b391db3c34780a3461f082f1147971203dc0710
Mikias Berhanu <bmikias482@gmail.com>
2025-04-17 13:00:02 +0300

    test config

--- Frontend_Staff/jest.config.cjs
+++ Frontend_Staff/jest.config.cjs
@@ -1,39 +1,21 @@
 module.exports = {
-  // Test environment (browser-like)
   testEnvironment: 'jest-environment-jsdom',
-
-  // Setup files (e.g., for @testing-library/jest-dom)
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-
-  // Where Jest looks for modules
   moduleDirectories: ['node_modules', '<rootDir>/src'],
-
-  // File transformations
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Handles JS/JSX/TS/TSX
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-
-  // Ignore node_modules (except for exceptions)
   transformIgnorePatterns: [
-    '/node_modules/(?!your-lib-to-transform)', // Add libs needing Babel
+    '/node_modules/(?!your-lib-to-transform)',
   ],
-
-  // Mock static assets and CSS modules
+  
   moduleNameMapper: {
-    // Fix "@/" alias resolution (matches your Vite config)
     '^@/(.*)$': '<rootDir>/src/$1',
-
-    // Mock styles and assets
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
 
-  // File extensions Jest recognizes
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
-
-  // Reset mocks between tests
   clearMocks: true,
-
-  // Coverage reports
   coverageDirectory: 'coverage',
 };
--- Frontend_Staff/src/setupTests.js
+++ Frontend_Staff/src/setupTests.js
@@ -1,12 +1,8 @@
-import fetchMock from 'jest-fetch-mock';
-import { TextEncoder, TextDecoder } from 'util';
+import fetchMock from "jest-fetch-mock";
+import { TextEncoder, TextDecoder } from "util";
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-fetchMock.enableMocks(); // Enable fetch mocking
-
-// Keep this line too:
-import '@testing-library/jest-dom';
-
-// Any other global setup...
-import '@testing-library/jest-dom';
+fetchMock.enableMocks();
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom";
